f17ba8120ffb1e27317545ff5082b343342dac1f
Tomislav <>
2024-02-15 15:47:06 +0100

    App name changed to books

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'IceAndFireApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('IceAndFireApp');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('IceAndFireApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BooksApp app is running!');
   });
 });
